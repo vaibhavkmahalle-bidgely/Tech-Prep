42c42a439d0603ab0e5c3bea0e464f5b9ac61a9b
Vaibhav Mahalle <vaibhavmahalle@bidgely.com>
2025-01-05 19:19:20 +0530

    Added the explaination for private Static Nested Class

--- src/StaticNested.java
+++ src/StaticNested.java
@@ -13,8 +13,23 @@ class OuterClass {
     }
 }
 
+class OuterClass1 {
+    private static class PrivateNestedClass{
+        public void print(){
+            System.out.println("This is nested class but with private keyword");
+        }
+    }
+
+    public void accessPrint(){
+        PrivateNestedClass privateNestedObj = new PrivateNestedClass();
+        privateNestedObj.print();
+    }
+
+}
+
 public class StaticNested {
     // Access specifier for class is package private by default : accessible within the same package, or public.
+    // But nested classes can be created as public, private, protected anything.
 
     // Class within another class is called Nested Class
     // If a Class B is only going to be used by Class A then we can create Class B inside Class A to avoid creating separate class file for the same.
@@ -25,8 +40,11 @@ public class StaticNested {
     // Two types : Static Nested & Non-Static Nested Class
     // Non-Static : Member Inner, Local Inner, Anonymous Inner
     public static void main(String[] args) {
+        // here you didn't need Object of OuterClass to access the NestedClass that's bcoz it's static.
         OuterClass.NestedClass NestedObj = new OuterClass.NestedClass();
         NestedObj.print();
-        // here you didn't need Object of OuterClass to access the NestedClass that's bcoz it's static.
+
+        OuterClass1 OuterClassObj = new OuterClass1();
+        OuterClassObj.accessPrint();
     }
 }
