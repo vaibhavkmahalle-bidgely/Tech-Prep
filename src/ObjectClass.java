d8622e858533ec2aa9ff6c3f2a09027574967292
Vaibhav Mahalle <vaibhavmahalle@bidgely.com>
2025-01-05 17:53:39 +0530

    Updating the Object Class

---
+++ src/ObjectClass.java
@@ -0,0 +1,22 @@
+public class ObjectClass {
+    private String name ;
+    private int age ;
+
+    // Constructor
+    public ObjectClass(String name, int age){
+        this.name = name ;
+        this.age = age ;
+    }
+
+    // Overriding toString() method from Object class
+    @Override
+    public String toString(){
+        return "Name : "+ name + ", age : "+age+".";
+    }
+
+    public static void main(String[] args) {
+        ObjectClass obj = new ObjectClass("Vaibhav", 26);
+
+        System.out.println(obj.toString());
+    }
+}
