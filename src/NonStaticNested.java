ffb522fed8fa25e9907037a2b5ca2e6385f896a3
Vaibhav Mahalle <vaibhavmahalle@bidgely.com>
2025-01-06 10:55:05 +0530

    Learnt about LocalInnerClass

--- src/NonStaticNested.java
+++ src/NonStaticNested.java
@@ -2,10 +2,24 @@ class NonStaticOuterClass {
     int instanceVariable = 10;
     static int classVariable = 20;
 
-    class InnerClass {
+    class MemberInnerClass {
         public void print() {
             System.out.println("InstanceVariable : " + instanceVariable + " ClassVariable : " + classVariable);
         }
+    }
+
+    void display(int num) {
+
+        // Local Inner Class is defined within the method, constructor or a block. It is accessible only within the scope it is defined.
+        // It cannot be declared private, public or protected.
+        class LocalInnerClass {
+            void printVal(){
+                System.out.println("Number for inner class : "+ num);
+            }
+        }
+        LocalInnerClass LocalInnerObj = new LocalInnerClass();
+        LocalInnerObj.printVal();
+
     }
 }
 
@@ -14,9 +28,11 @@ public class NonStaticNested {
     public static void main(String[] args) {
 
         NonStaticOuterClass NonStaticOuterObj = new NonStaticOuterClass();
-        NonStaticOuterClass.InnerClass InnerObj = NonStaticOuterObj.new InnerClass();
+        NonStaticOuterClass.MemberInnerClass InnerObj = NonStaticOuterObj.new MemberInnerClass();
+        // here we are creating InnerObj using the object of the parent.
 
         InnerObj.print();
+        NonStaticOuterObj.display(55);
     }
 
 
