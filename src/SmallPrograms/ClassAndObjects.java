46dd1a77cb0b02720a3d7380021016d20b929d5c
Vaibhav Mahalle <vaibhavmahalle@bidgely.com>
2025-01-07 13:47:20 +0530

    Java classes and objects

---
+++ src/SmallPrograms/ClassAndObjects.java
@@ -0,0 +1,45 @@
+package SmallPrograms;
+
+class Box {
+    private int length, width, height;
+
+    public void setDimensions(int l, int b, int h) {
+        length = l;
+        width = b;
+        height = h;
+    }
+
+    public void showDimensions() {
+        System.out.println("Dimensions of box are : L, B, H : " + length + " , " + width + " , " + height);
+    }
+}
+
+public class ClassAndObjects {
+    // if I want to store the data of different data types but with some relation between them, e.g. Student
+    // here the relation between them is established bcoz they belong to same class.
+
+    // Class is a description of object's property and behaviour.
+    // creating a class is as good as creating a data type.
+    // class is defining category of data.
+
+    // Object is a real world entity
+    // Object is an instance of a class
+    // Object consumes memory to hold property values.
+    // bada variable hi hai object
+
+    // In java we make objects dynamically only.
+
+    // variable obj is pointing to the object of Class Box
+    public static void main(String[] args) {
+        Box obj = new Box();
+        obj.setDimensions(2, 3, 4);
+        obj.showDimensions();
+
+        // here obj will point to new object of Class Box, hence the values assigned to it will be gone.
+        obj = new Box();
+        obj.showDimensions();
+
+    }
+
+
+}
