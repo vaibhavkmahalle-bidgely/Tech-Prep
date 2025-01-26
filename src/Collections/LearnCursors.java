56179f0c8ee06fe7235d4d6e843afd0520bb3833
Vaibhav Mahalle <vaibhavmahalle@bidgely.com>
2025-01-26 09:42:33 +0530

    Learnt about cursors like iterator, listIterator

---
+++ src/Collections/LearnCursors.java
@@ -0,0 +1,38 @@
+package Collections;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class LearnCursors {
+    /*
+        Cursors are used to retrieve data from collection object one by one,
+        Three cursors : Enumerator, Iterator, ListIterator
+
+        Enumerator : used for Stack and Vector as they are 1.0 : offers hasNextElements(), next() - unidirectional
+        Iterator : offers three methods - hasNext(), next(), remove() - unidirectional
+        ListIterator : bidirectional, child interface of Iterator
+
+        All List implemented classes provide a method called listIterator()
+        public ListIterator listIterator()
+
+        ListIterator methods :
+                public boolean hasNext();
+                public Object next();
+                public boolean hasPrevious();
+                public Object previous();
+                public int nextIndex();
+                public int previousIndex();
+                public void remove();
+                public void add(E e);
+                public void set(E e);
+
+
+    */
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(2);
+
+        Iterator<Integer> itr = list.iterator();
+        System.out.println("Iterator "+ itr.next());
+    }
+}
---
+++ src/DSA/LinkedListInsertionAtStart.java
@@ -0,0 +1,68 @@
+package DSA;
+
+import org.w3c.dom.ls.LSOutput;
+
+class ListNode1 {
+    int val;
+    ListNode next;
+
+    ListNode1() {
+        val = 0;
+        next = null;
+    }
+
+    ListNode1(int data1) {
+        val = data1;
+        next = null;
+    }
+
+    ListNode1(int data1, ListNode next1) {
+        val = data1;
+        next = next1;
+    }
+}
+
+class Solutions1 {
+    public ListNode insertAtStart(ListNode head, int X, int K) {
+        if (head == null) {
+            return new ListNode(X);
+        }
+        ListNode temp = head;
+        if (K == 1) {
+            temp = new ListNode(X, head);
+            return temp;
+        }
+
+        while (K > 2) {
+            head = head.next;
+            K--;
+        }
+        head.next = new ListNode(X,head.next);
+
+        return temp;
+    }
+}
+
+public class LinkedListInsertionAtStart {
+
+    public static void main(String[] args) {
+        Solutions1 obj = new Solutions1();
+
+        ListNode1 node1 = new ListNode1(3, null);
+
+        System.out.println("new Node is value and next " + node1.val + node1.next);
+
+        System.out.println("Insert node at start : ");
+        ListNode head = obj.insertAtStart(null, 44, 1);
+
+        System.out.println("Printing head : and next : " + head.val + " " + head.next);
+
+        System.out.println("Insert node at start : ");
+        ListNode head1 = obj.insertAtStart(head, 45, 1);
+        ListNode head2 = obj.insertAtStart(head1, 46, 2);
+
+        System.out.println("Printing head : and next : " + head2.val + " " + head2.next.val + " " + head2.next.next.val);
+
+
+    }
+}
