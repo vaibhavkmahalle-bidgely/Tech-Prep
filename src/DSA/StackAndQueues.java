70bdadcab54769267cd02dc421abe96a8e121a7b
Vaibhav Mahalle <vaibhavmahalle@bidgely.com>
2025-02-13 14:18:51 +0530

    pushing older code

--- src/DSA/StackAndQueues.java
+++ src/DSA/StackAndQueues.java
@@ -3,9 +3,53 @@ package DSA;
 import java.io.PrintStream;
 import java.util.*;
 
+class StackQueue {
+    // declare a stack then use the methods of stack to mimic the queue methods.
+
+    ArrayDeque<Integer> stack1 = new ArrayDeque<>();
+    int top = -1;
+
+    public StackQueue() {}
+
+    public void push(int x) {
+        // when stack is empty
+        stack1.push(x);
+        top++;
+        int s = stack1.size();
+        int[] arr = new int[s];
+        for (int i = 0; i < s; i++) {
+            arr[i] = stack1.pop();
+            System.out.println("popping and storing arr i "+ arr[i]);
+        }
+        for(int i = 0; i < s ; i++){
+            stack1.push(arr[i]);
+            System.out.println("pushing arr i "+ arr[i]);
+        }
+
+        // when stack has few elements
+
+    }
+
+    public int pop() {
+        if(isEmpty()){
+            return -1;
+        }
+        return stack1.pop();
+    }
+
+    public int peek() {
+        return stack1.peek();
+    }
+
+    public boolean isEmpty() {
+        return stack1.isEmpty();
+    }
+}
+
 public class StackAndQueues {
     public static void main(String[] args) {
-        Stack<Integer> stack = new Stack<>();
+
+       /* Stack<Integer> stack = new Stack<>();
 
         stack.push(5);
         stack.push(6);
@@ -46,7 +90,14 @@ public class StackAndQueues {
         System.out.println("size of queue :: " + queue.size());
         System.out.println("Dequeue operation :: " + queue.remove());
         System.out.println("peeking in queue :: " + queue.peek());
-        System.out.println("size of queue :: " + queue.size());
+        System.out.println("size of queue :: " + queue.size());*/
+
+        StackQueue stack = new StackQueue();
+
+        stack.push(3);
+        stack.push(5);
+
+        System.out.println("stack is : "+stack.pop());
 
     }
 }
