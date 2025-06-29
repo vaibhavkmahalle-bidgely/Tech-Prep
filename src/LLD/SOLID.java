44c62aff043b358bcdcd528c026126024f8ec29e
Vaibhav Mahalle <vaibhavmahalle@bidgely.com>
2025-06-29 19:55:59 +0530

    SOLID : Open/Close principle

--- src/LLD/SOLID.java
+++ src/LLD/SOLID.java
@@ -2,6 +2,8 @@ package LLD;
 
 public class SOLID {
     // S - Single Responsibility Principle : A class should have only one reason to change.
+    // O - Open for Extension but closed for Modification
+    // Don't modify Already tested classes if needed extend them.
 
     static class Marker{
         String name;
@@ -16,7 +18,6 @@ public class SOLID {
            this.price = price;
        }
     }
-
     // Here Invoice class needs to be changed each time any of the calulateTotal, printTotal, saveToDB is changed so that class does not follow single Responsibility principle.
     // Break the current class in three subclasses InvoiceDao , printTotal, calculateTotal then it will follow single responsibility
     static class Invoice{
@@ -43,49 +44,65 @@ public class SOLID {
     }
 
     static class InvoiceTotal{
-        private  Marker marker;
-        private int quantity;
+        Invoice invoice;
 
-        public InvoiceTotal(Marker marker, int quantity){
-            this.marker = marker;
-            this.quantity = quantity;
+        public InvoiceTotal(Invoice invoice){
+            this.invoice = invoice;
         }
 
         public int calulateTotal(){
-            int price = ((marker.price)*this.quantity);
+            int price = ((invoice.marker.price)*invoice.quantity);
             return price;
         }
 
     }
 
-    static class InvoiceDAO{
-        private  Marker marker;
-        private int quantity;
+    static class DBInvoiceDAO implements InvoiceDAO{
+        Invoice invoice;
 
-        public InvoiceDAO(Marker marker, int quantity){
-            this.marker = marker;
-            this.quantity = quantity;
+        public DBInvoiceDAO(Invoice invoice){
+            this.invoice = invoice;
         }
 
-        public void saveToDB(){
-
+        public void save(Invoice invoice){
+            // save to DB
         }
+
+        // Adding new method to InvoiceDao is not recommended
+        public void saveToFile(){
+            // PRONE TO ERRORS
+        }
+    }
+
+    static class FileInvoiceDAO implements InvoiceDAO{
+        Invoice invoice;
+
+        public FileInvoiceDAO(Invoice invoice){
+            this.invoice = invoice;
+        }
+
+        public void save(Invoice invoice){
+            // save to File
+        }
+
+
+    }
+
+    interface InvoiceDAO{
+        public void save(Invoice invoice);
     }
 
     static class PrintInvoiceTotal{
-        private  Marker marker;
-        private int quantity;
+        Invoice invoice;
 
-        public PrintInvoiceTotal(Marker marker, int quantity){
-            this.marker = marker;
-            this.quantity = quantity;
+        public PrintInvoiceTotal(Invoice invoice){
+            this.invoice = invoice;
         }
 
         public void printTotal(){
-            System.out.println("total is "+ ((marker.price)*this.quantity));
+            System.out.println("total is "+ ((invoice.marker).price)* invoice.quantity);
         }
     }
-
 
     public static void main(String[] args) {
         Invoice inv = new Invoice(new Marker("vaibhav","green",2025,25), 2);
